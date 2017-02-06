acb80eaa22f66e9c84772c91633c118ce0af1397
Stein <izaya256@gmail.com>
2017-02-06 12:36:39 +0200

    my second commit

--- Student.js
+++ Student.js
@@ -1,7 +1,9 @@
-function Student (name, gender, skype) {
+function Student (name, surname, gender, skype, phone) {
   this.name = name;
+  this.surname = surname;
   this.gender = gender;
   this.skype = skype;
+  this.phone = phone;
   
   return this;
 }
